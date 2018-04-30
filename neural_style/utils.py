8a6790865c237f2a2533063d6077dee3368210de
PEAcademy <mcheich@gmail.com>
2018-04-29 23:27:29 -0400

    Alter save image

--- neural_style/utils.py
+++ neural_style/utils.py
@@ -16,7 +16,8 @@ def save_image(filename, data):
     img = data.clone().clamp(0, 255).numpy()
     img = img.transpose(1, 2, 0).astype("uint8")
     img = Image.fromarray(img)
-    img.save("filename.jpg")
+    #img.save("{}.jpg".format(filename))
+    img.save("filename")
 
 
 def gram_matrix(y):
